92f0f6acbe7c2cb99518b8736db811c6a7a11d75
Carson <carsontwj@gmail.com>
2023-01-02 15:02:36 -0500

    Fix bug were warning message multiplies on click
    Warning messages continue to be created and added to the dom. Fix bug to
    prevent that

--- javascript.js
+++ javascript.js
@@ -36,20 +36,47 @@ submitButton.addEventListener('click', () => {
     deleteContent(title, author, pages, read);
 });
 
+
 //checks if input boxes are filled
 function checkInput(title, author, pages) {
-    if (title.textContent <= 0) {
-        console.log('test');
-        const warning = document.createElement('div');
-        warning.classList.add('warning');
-        warning.textContent = "Fill out the box above!"
-
-        title.appendChild(warning);
+    if (title.value === '') {
+        //select the parent node and add warning message if input is emtpy
+        const parent = document.querySelector('.title')
+        //select warning div
+        const warningTitle = document.querySelector('.title > .warning')
+        //if warning div does not exist creat and add one
+        if (!warningTitle) {
+            parent.appendChild(warning());
+        }
     }
+    if (author.value === ''){
+        const parent2 = document.querySelector('.author')
+        const warningAuthor = document.querySelector('.author > .warning')
+        if (!warningAuthor) {
+            parent2.appendChild(warning())
+        }
+    }
+    if (pages.value <= 0) {
+        const parent3 = document.querySelector('.pages') 
+        const warningPages = document.querySelector('.pages > .warning')
+        if (!warningPages){
+            parent3.appendChild(warning())
+        }
+    }
+}
+
+//creates a node when user hasn't entered anything
+function warning() {
+    const warning = document.createElement('div');
+    warning.classList.add('warning');
+    warning.textContent = "Fill in the box!"
+    warning.style.cssText = "color: red; font-size: 0.75rem;";
+    return warning;
 }
 
 //Deletes the content in input boxes
 function deleteContent(title, author, pages, read) {
+    //set all values to null and checkbox to false
     title.value = null;
     author.value = null;
     pages.value = null;
