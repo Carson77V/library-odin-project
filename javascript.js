fc260b1739077b056552253df39893ea60093f70
Carson <carsontwj@gmail.com>
2023-01-02 14:11:19 -0500

    Create function that erases data from input
    function resets checkmark and all other inputs in the form

--- javascript.js
+++ javascript.js
@@ -26,4 +26,32 @@ const submitButton = document.querySelector('.submit > button');
 submitButton.addEventListener('click', () => {
     //prevent the button from submitting anything
     event.preventDefault();
+    //Select all the dom elements that will be saved
+    let title = document.querySelector('#title');
+    let author = document.querySelector('#author');
+    let pages = document.querySelector('#pages');
+    let read = document.querySelector('#read');
+
+    checkInput(title, author, pages);
+    deleteContent(title, author, pages, read);
 });
+
+//checks if input boxes are filled
+function checkInput(title, author, pages) {
+    if (title.textContent <= 0) {
+        console.log('test');
+        const warning = document.createElement('div');
+        warning.classList.add('warning');
+        warning.textContent = "Fill out the box above!"
+
+        title.appendChild(warning);
+    }
+}
+
+//Deletes the content in input boxes
+function deleteContent(title, author, pages, read) {
+    title.value = null;
+    author.value = null;
+    pages.value = null;
+    read.checked = false;
+}
